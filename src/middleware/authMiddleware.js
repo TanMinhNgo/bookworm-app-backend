3fe373a18d85fcfbcbc5ca58ce122943d6bc76f4
NgoMinhTan <ngominhtan1252004@gmail.com>
2025-09-19 16:20:41 +0700

    sadsadaw

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -3,16 +3,12 @@ import User from '../models/User.js';
 
 const protectRoute = async (req, res, next) => {
     try {
-        let token = req.headers["Authorization"];
-        if (token) {
-            token = token.replace("Bearer ", "");
+        let {Authorization} = req.headers;
+        if (Authorization) {
+            Authorization = Authorization.replace("Bearer ", "");
         }
 
-        if (!token) {
-            return res.status(401).json({ message: 'Unauthorized' });
-        }
-
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const decoded = jwt.verify(Authorization, process.env.JWT_SECRET);
         const user = await User.findById(decoded.id);
 
         if (!user) {
